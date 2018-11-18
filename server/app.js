8a3cd39335a0df23b05b537cafd8373affbd2c57
Flynn Gurnsey <flynn.gurnsey@gmail.com>
2018-11-17 20:55:39 -0500

    Add favicon

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const favicon = require('serve-favicon');
 const path = require('path');
 
 const app = express();
 
+app.use(favicon(path.join(__dirname, '../client', 'img', 'favicon.ico')));
 app.use('/', express.static(path.join(__dirname, '../client')));
 
 app.listen(3000, () => {
